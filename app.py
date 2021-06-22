4f37cd53fae72160f4d9a35d7b9a9b448119417c
Carrie Mathieu <carrieburks@gmail.com>
2021-06-21 23:08:07 -0400

    added employeeArray

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+employeeArray = [
+    {
+        "id":1, 
+        "employee_name":"Tiger Nixon", 
+        "employee_salary":320800, 
+        "employee_age":61
+    }, {
+        "id":2, 
+        "employee_name":"Garrett Winters", 
+        "employee_salary":170750, 
+        "employee_age":63
+    }, {
+        "id":3, 
+        "employee_name":"Ashton Cox", 
+        "employee_salary":86000, 
+        "employee_age":66
+}]
 
+# *** GET endpoint ***
+# - takes as input an employee’s ID 
+# - returns the matching employee’s name, salary, and age in JSON.
+
+# *** GET endpoint ***
+# - returns an array of all employees
+# - sorted by highest salary to lowest
+
+# *** POST endpoint ***
+# - takes as input a JSON object containing the following fields:
